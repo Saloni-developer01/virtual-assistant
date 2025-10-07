44263ffb8cadcbb922443ada906fee1a5e5d5358
unknown <webhostingportfolio124@gmail.com>
2025-10-07 15:00:20 +0530

    modified backend url

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -7,7 +7,7 @@ export const userDataContext = createContext();
 
 function UserContext ({children}){
 
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://virtual-assistant-backend-u8xy.onrender.com"
     const [userData, setUserData] = useState(null);
     const [frontendImage, setFrontendImage] = useState(null);
     const [backendImage, setBackendImage] = useState(null);
