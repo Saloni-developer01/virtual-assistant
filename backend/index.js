8df07f8eef55bfc9672ed146f012ca5a19287357
unknown <webhostingportfolio124@gmail.com>
2025-10-07 15:16:43 +0530

    modified frontend url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import geminiResponse from './gemini.js';
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-0o83.onrender.com",
     credentials: true
 }));
 
